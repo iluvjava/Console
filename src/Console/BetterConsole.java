c564027a8ee1ab80152c7765504ccdff8aeff507
iluvjava <victor1301166040@gmail.com>
2018-12-07 23:10:47 -0800

    nz

--- src/Console/BetterConsole.java
+++ src/Console/BetterConsole.java
@@ -1,5 +1,6 @@
 package Console;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,14 +9,44 @@ public class BetterConsole implements Runnable
 	private boolean flag = false; // True means stop refreshing. 
 	
 	protected List<String> slst = new LinkedList<>();
+	
 	public BetterConsole(String...strings)
+	{
+		for(String s : strings)
+		{
+			slst.add(String.format("%1$"+Console.WIDTH+"s", s).replace('\n', ' '));
+		}
+	}
+	
+	public void run() 
+	{
+		while(!flag)
+		{
+			
+		}
+	}
+	
+	private void printAll()
+	{
+		{
+			String s = null;
+			for(Iterator<String> itr =slst.iterator();itr.hasNext();)
+			{
+				s = itr.next();
+				Console.console.println(s);
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * @param index
+	 * The index of line 
+	 */
+	public synchronized void set(int index)
 	{
 		
 	}
-	public void run() {
-		
-	}
-	
 	
 	
 	
