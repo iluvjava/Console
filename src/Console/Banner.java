41cd7419120dc42bd6c317133c09d7c4b0348d80
iluvjava <victor1301166040@gmail.com>
2018-12-08 21:44:42 -0800

    na

--- src/Console/Banner.java
+++ src/Console/Banner.java
@@ -1,10 +1,29 @@
 package Console;
 
+
+/**
+ * The banners moves text on a single line horizontally. It should have good API and stuff to make it easy to use. 
+ * @author Administrator
+ *
+ */
 public class Banner implements Runnable{
 
+	private boolean flag = false; // true means stop refreshing. 
+	private String[] content; 
+	
+	
 	public void run() {
 		
 	}
 
 }
- 
+/**
+ * Provides an animated sequence for a certain input string.
+ * @author Administrator
+ *
+ */
+ class sequencer
+ {
+	 
+	 
+ }
